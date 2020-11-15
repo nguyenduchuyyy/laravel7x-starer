5dfeea51928b6e2e9ecc7d55c8f0858a76a6a5c3
nguyenduchuyyy <huy.nguyen@exe.com.vn>
2020-11-15 21:11:59 +0700

    Update templet

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,12 @@
 
                     @if(Auth::user())
                        <h1> Welcome {{Auth::user()->name}} </h1>
+                       <!-- <a href="{{route('products.index')}}" >Manage Products</a> -->
+                        <div class="navbar" style="font-size: 22px;">
+                            <a class="active" href="{{route('products.index')}}">Manage Products</a>
+                            <a href="#">Manage Contact</a>
+                            <a href="#">Manage user</a>
+                        </div>
                     @endif
         </div>
     </div>
